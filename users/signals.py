9b64b081f8d32a813425ba6ed66d8977b3a3e939
Doniyor401 <doniyortursunboyev000@bk.ru>
2024-03-12 17:09:29 +0500

    Use celery welcome email

--- users/signals.py
+++ users/signals.py
@@ -2,14 +2,14 @@ from users.models import CustomUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.mail import send_mail
+from users.tasks import send_email
 
 
 @receiver(post_save, sender=CustomUser)
 def send_welcome_email(sender, instance, created, **kwargs):
     if created:
-        send_mail(
+        send_email.delay(
             "Welcome to GoodReads Clone",
             f"Hi, {instance.username}. Welcome to GoodReads Clone. Enjoy the books and reviews",
-            "doniyortursunvoyev@gmail.com",
             [instance.email]
         )
